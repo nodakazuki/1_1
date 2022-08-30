c835d88d65b90a7a60ea82e80b177666637c5ed5
nodakazuki <kazuki07031999@icloud.com>
2022-08-30 12:54:07 +0900

    fifth commit

--- app/Post.php
+++ app/Post.php
@@ -3,15 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'title',
         'body',
     ];
     // これがないと保存できない
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
         // まずupdated_atの順番で並べた後、件数を5件に絞ってそれを取得する。
